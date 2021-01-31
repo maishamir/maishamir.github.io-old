5ad55e12528a7a6ec6758dfcb8221462645e488c
Maisha <maisha.mir2014@gmail.com>
2021-01-31 01:14:12 -0800

    added toggle button

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-let toggle = document.querySelector(".toggle-theme");
+// let toggle = document.querySelector(".toggle-theme");
+// document.body.classList.remove("teal");
+
+// toggle.addEventListener("click", function (e) {
+//   e.preventDefault();
+
+//   if (document.body.classList.contains("teal")) {
+//     // Turning the theme off:
+//     document.body.classList.remove("teal");
+
+//     // Reverse logic on the button text, so that users can turn
+//     // the theme back on:
+//     toggle.innerText = "Turn theme on";
+//   } else {
+//     document.body.classList.add("teal");
+//     toggle.innerText = "Turn theme off";
+//   }
+// });
+
+// // if()
+
+var checkbox = document.querySelector("input[type=checkbox]");
 document.body.classList.remove("teal");
 
-toggle.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  if (document.body.classList.contains("teal")) {
-    // Turning the theme off:
-    document.body.classList.remove("teal");
-
-    // Reverse logic on the button text, so that users can turn
-    // the theme back on:
-    toggle.innerText = "Turn theme on";
-  } else {
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
     document.body.classList.add("teal");
-    toggle.innerText = "Turn theme off";
+  } else {
+    document.body.classList.remove("teal");
   }
 });
-
-// if()
